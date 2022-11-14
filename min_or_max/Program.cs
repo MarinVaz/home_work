48d8c10052d59f4e55bd88670f5d34e995210f61
Marina <errorda@gmail.com>
2022-11-14 15:55:50 +0300

    homework min or max

---
+++ min_or_max/Program.cs
@@ -0,0 +1,22 @@
+﻿string input_1 = Console.ReadLine();
+string input_2 = Console.ReadLine();
+double a = Convert.ToInt32(input_1);
+double b = Convert.ToInt32(input_2);
+if (a>b)
+{
+  Console.WriteLine("max = " +a);  
+}
+else
+Console.WriteLine("max = " + b);
+/*string input_1 = Console.ReadLine();
+string input_2 = Console.ReadLine();
+
+int number_1 = Convert.ToInt32(input_1);
+int number_2 = Convert.ToInt32(input_2);
+int result = number_2 * number_2;
+if (result == number_1)
+{
+Console.Write("ДА");
+}
+else
+Console.Write("НЕТ");*/
