fb91e725d091777080ac4f30c296ac5b28381b56
Marina <errorda@gmail.com>
2022-11-14 17:01:15 +0300

    even 1 to n

---
+++ even_1_to_n/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число : ");
+string input_1 = Console.ReadLine();
+int n = Convert.ToInt32(input_1);
+int i = 1;
+while (i<=n)
+    if (i % 2 == 0)
+    {
+    Console.WriteLine(i); 
+    i++;
+    }
+    else 
+i++; 
