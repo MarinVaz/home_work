020f61d53f78a93aa27f9222496823b3cb88fe0a
Marina <errorda@gmail.com>
2022-11-14 16:36:53 +0300

    even or not even

---
+++ even/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число : ");
+string input_1 = Console.ReadLine();
+double a = Convert.ToInt32(input_1);
+if (a % 2 == 0)
+    Console.WriteLine("четное"); 
+    else 
+    Console.WriteLine("нечетное"); 
+        
