abb7293d2d6e3687c5323c474763ef14fa15c9e2
Marina <errorda@gmail.com>
2022-11-14 16:24:37 +0300

    find max

---
+++ max/Program.cs
@@ -0,0 +1,12 @@
+﻿string input_1 = Console.ReadLine();
+string input_2 = Console.ReadLine();
+string input_3 = Console.ReadLine();
+double a = Convert.ToInt32(input_1);
+double b = Convert.ToInt32(input_2);
+double c = Convert.ToInt32(input_3);
+if (a > b && a > c)
+    Console.WriteLine("Max = " + a); 
+    else if (b > c)
+    Console.WriteLine("Max = " + b); 
+        else
+        Console.WriteLine("Max = " + c); 
